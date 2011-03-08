64eb0a04b3c87e7d59fc02323f6fb010ddae1791
Keenan Brock <kbrock@patientslikeme.com>
2011-03-08 17:49:38 -0500

    trying to cache everything

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,14 +1,16 @@
 class AvatarsController < ApplicationController
-  MOD_DATE = DateTime.parse('2011-1-1T120000').utc
+  MOD_DATE = Time.parse('2011-1-1T120000').utc
+  ETAG=MOD_DATE.hash
   def index
   end
   
   def show
-    #if stale? (:last_modified => MOD_DATE)
+    expires_in 1.week, 'max-stale' => 2.weeks, :public => true
+    if stale? (:last_modified => MOD_DATE, :etag => MOD_DATE)
       a = Avatar.find(params[:id])
       a.generate
-      send_file a.filename, :type => a.filetype, :disposition => 'inline'
+      send_file a.filename, :type => a.filetype, :url_based_filename => true, :disposition => 'inline'
       a.cleanup
-    #end
+    end
   end
 end
