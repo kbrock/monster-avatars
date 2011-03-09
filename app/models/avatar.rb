adaf06a3f2d48b3a4ad6889afd70e1b569d9d4e7
Keenan Brock <kbrock@patientslikeme.com>
2011-03-09 04:45:21 -0500

    removed basic parts, and parts_s (styled)

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -3,33 +3,16 @@ class Avatar
   #These are in a particular order
   ALL_PARTS=[:legs, :hair, :arms, :body, :eyes, :mouth]
 
-  # PARTS_DIR='parts'
-  # NUM_PARTS={
-  #  :legs  =>  5,
-  #  :hair  =>  5,
-  #  :arms  =>  5,
-  #  :body  => 15,
-  #  :eyes  => 15,
-  #  :mouth => 10
-  # }
-  # PARTS_DIR='parts_s'
-  # NUM_PARTS={
-  #  :legs  =>  13,
-  #  :hair  =>  7,
-  #  :arms  =>  9,
-  #  :body  =>  5,
-  #  :eyes  =>  5,
-  #  :mouth =>  7
-  # }
-    PARTS_DIR='parts_c'
-    NUM_PARTS={
-     :legs  =>  18,
-     :hair  =>  12,
-     :arms  =>  14,
-     :body  =>  20,
-     :eyes  =>  20,
-     :mouth =>  17
-    }
+  PARTS_DIR='parts_c'
+  NUM_PARTS={
+   :legs  =>  18,
+   :hair  =>  12,
+   :arms  =>  14,
+   :body  =>  20,
+   :eyes  =>  20,
+   :mouth =>  17
+  }
+
   PART_COLOR = {
     'arms_13.png'  => :body,
     'legs_6.png'   => :body,
@@ -37,20 +20,6 @@ class Avatar
     'mouth_15.png' => :body,
     'mouth_14.png' => :body
   }
-  #   'hair_9.png'   => [  0.6, 0.75], 'arms_7.png'   => [ 0.01, 0.05],
-  #   'hair_10.png'  => [ 0.01, 0.05], 'mouth_9.png'  => [ 0.01, 0.05],
-  #   'mouth_6.png'  => [ 0.01, 0.05], 'mouth_12.png' => [ 0.01, 0.05],
-  #   'arms_1.png' => :white,'arms_2.png' => :white,'arms_9.png' => :white,'arms_10.png' => :white,
-  #   'eye_13.png' => :white,
-  #   'hair_1.png' => :white,'hair_2.png' => :white,'hair_3.png' => :white,'hair_5.png' => :white,
-  #   'legs_4.png' => :white,'legs_S11.png => :white',
-  #   'arms_3.png' => :random,'arms_4.png' => :random,'arms_5.png' => :random,'arms_6.png' => :random,'arms_8.png' => :random,'arms_10.png' => :random,'arms_11.png' => :random,
-  #   'arms_12.png' => :random,'arms_14.png' => :random,
-  #   'hair_6.png'  => :random,'hair_7.png' => :random,'hair_8.png' => :random,'hair_9.png' => :random,
-  #   'legs_1.png' => :random,'legs_2.png' => :random,'legs_3.png' => :random,'legs_5.png' => :random,'legs_6.png' => :random,'legs_7.png' => :random,
-  #   'legs_8.png' => :random,'legs_9.png' => :random,'legs_11.png' => :random,'legs_12.png' => :random,'legs_15.png' => :random,'legs_17.png' => :random,
-  #   'mouth_3.png' => :random,'mouth_4.png' => :random,'mouth_7.png' => :random,'mouth_10.png' => :random,'mouth_16.png' => :random
-  #   }
 
   attr_accessor :filename, :key
 
@@ -58,7 +27,6 @@ class Avatar
     @force = false
     @key = key.to_i
     @filename="#{Rails.root}/tmp/avatar-#{@key}.png"
-    #@filename="#{Rails.root}/sample.png"
   end
 
   def generate
@@ -83,11 +51,12 @@ class Avatar
     a = Avatar.new(id)
   end
 
+  private
   def open
     `open #{@filename}`
+    self
   end
 
-  private
   def command
     "cd #{Rails.root} ; convert parts/background.png " + (part_files + [@filename]).join(' -composite ')
   end
