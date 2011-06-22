2e9db60a344864975f13482dd4c487c349d5055c
Keenan Brock <kbrock@patientslikeme.com>
2011-06-21 22:25:35 -0400

    color tool tweaks. storing the values

--- app/models/color_tool.rb
+++ app/models/color_tool.rb
@@ -3,27 +3,31 @@ require 'color'
 class ColorTool
   attr_accessor :base
   def initialize(base=nil)
-    @base=base||random
+    @colors={}
+    @colors[:body]=base||random
+  end
+
+  def add_color(name,value=nil)
+    @colors[name]=color(value)
   end
 
   def color(value=nil)
+    return @colors[value] if @colors[value]
     case value
-    when :body
-      @base
     when :random
       random
     when String
       value
     when Array
-      random(value)
+      random(value[0],value[1])
     else
       random
     end
   end
 
   #hsb stuff
-  def random(value=nil)
-    Color::HSL.from_fraction(rand, 0.8, rand_float(0.2,0.32)).html
+  def random(hue=nil,brightness=nil)
+    Color::HSL.from_fraction(hue||rand, 0.8, brightness||rand_float(0.2,0.32)).html
   end
 
   def rand_float(min, max)
