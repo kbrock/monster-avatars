d1388ae4ff5e237275bd62fdd83cdc4bd0a66955
Keenan Brock <kbrock@patientslikeme.com>
2011-06-23 00:32:22 -0400

    avatar work with ids (default) or monster string

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,13 +2,17 @@ class AvatarsController < ApplicationController
   MOD_DATE = Time.parse('2011-1-1T120000').utc
   ETAG=MOD_DATE.hash
   def index
+    redirect_to '/'
   end
   
   def show
+    mode = params[:mode]||'id'
+    id = params[:id]
+    id = id.to_i if mode == 'id'
     force=Rails.env.development?
     expires_in 1.week, 'max-stale' => 2.weeks, :public => true unless force
     if force || stale?(:last_modified => MOD_DATE, :etag => MOD_DATE)
-      @avatar = Avatar.find(params[:id])
+      @avatar = Avatar.find(id)
       @avatar.generate(force)
       send_file @avatar.filename, :type => @avatar.filetype, :url_based_filename => true, :disposition => 'inline'
       @avatar.cleanup
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Monsters::Application.routes.draw do
   #root :to => "welcome#index"
   resources :avatars
+  resources :monsters, :to => "avatars", :mode => 'key'
 end
